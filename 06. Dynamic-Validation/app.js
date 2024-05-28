e79965f8c8b97409d1de366837792814f772d082
miroslavnanovski <156704544+miroslavnanovski@users.noreply.github.com>
2024-05-28 03:11:24 +0300

    Added functionality
    added an event listener to validate the input text

--- 06. Dynamic-Validation/app.js
+++ 06. Dynamic-Validation/app.js
@@ -1,3 +1,20 @@
-function validate() {
+
+    function validate() {
+      
+        const pattern = /[a-z]+@[a-z]+.[a-z]+/g;
+        const inputField = document.getElementById('email');
+       
     
-}
+        inputField.addEventListener('change', function(event) {
+            
+            let inputValue = inputField.value;
+    
+            if(!pattern.test(inputValue)){
+                inputField.classList.add('error');
+                
+            } else {
+                inputField.classList.remove('error')
+            }
+    
+        })
+    }
