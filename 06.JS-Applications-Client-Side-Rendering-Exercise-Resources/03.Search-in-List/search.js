7eafda780284e7d9abb560934913d560e712d7b5
miroslavnanovski <156704544+miroslavnanovski@users.noreply.github.com>
2024-07-17 01:55:01 +0300

    Completed 5/6 exercises

--- 06.JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
+++ 06.JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
@@ -4,12 +4,16 @@ import {render,html} from "./node_modules/lit-html/lit-html.js"
 const inputRef = document.getElementById('searchText');
 const townsDiv = document.getElementById('towns');
 const seachBtn = document.querySelector('button');
+const resultDiv = document.getElementById('result')
 
 seachBtn.addEventListener('click',search);
 
+
+let matches = 0;
+
 search()
 function search(e) {
-
+   matches = 0;
    let searchText = null;
    if(e){
       searchText = inputRef.value;
@@ -17,12 +21,18 @@ function search(e) {
 
    let temp = towns.map(town => createTemp(town,searchText));
    render(temp,townsDiv)
+   if(matches >= 0){
+      resultDiv.textContent = `${matches} matches found`;
+   }
+   
 
 }
 
 function createTemp(town,searchText){
-
-   let isMatch = town.includes(searchText);
+   let isMatch = town.includes(searchText) && searchText !== '';
+   if(isMatch){
+      matches++;
+   }
    return html`
    <ul>
       <li class=${isMatch ? "active" : ""}>${town}</li>
--- 06.JS-Applications-Client-Side-Rendering-Exercise-Resources/04.Fill-Dropdown/dropdown.js
+++ 06.JS-Applications-Client-Side-Rendering-Exercise-Resources/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,42 @@
-function addItem() {
-    console.log('TODO:...');
+import { api } from "./requester.js";
+import {html,render} from "./node_modules/lit-html/lit-html.js"
+
+const menuItems = document.getElementById('menu');
+const textInput = document.getElementById('itemText');
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    const addBtn = form.querySelector('input[type="submit"]')
+    const textInput = form.querySelector('input[type="text"]')
+
+    addBtn.addEventListener('click',addItem(textInput.value));
+})
+
+
+window.onload = (event) => {
+    initialGet()
+  };
+const URL = 'http://localhost:3030/jsonstore/advanced/dropdown';
+
+async function initialGet(){
+    let data = await api.get(URL)
+    data = Object.values(data);
+    render(createOption(data),menuItems)
+   
+}
+
+async function addItem(text) {
+    text = {
+        text
+    }
+    await api.post(URL,text);
+    textInput.value = ''
+    initialGet();
+
+}
+
+function createOption(data){
+    return data.map(item => html`<option value=${item._id}>${item.text}</option>`)
 }
