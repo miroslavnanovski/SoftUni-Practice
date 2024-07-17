8fe7672e60c7539c8b2888e8bcc6da7de85eecd3
miroslavnanovski <156704544+miroslavnanovski@users.noreply.github.com>
2024-07-17 18:28:41 +0300

    started working on 6th exercise

--- 06.JS-Applications-Client-Side-Rendering-Exercise-Resources/06.Book-Library/app.js
+++ 06.JS-Applications-Client-Side-Rendering-Exercise-Resources/06.Book-Library/app.js
@@ -1,1 +1,65 @@
-console.log('My requests...')
+import { render, html } from "./node_modules/lit-html/lit-html.js";
+import { api } from "./requester.js";
+
+const loadBtn = document.getElementById('loadBooks');
+const URL = 'http://localhost:3030/jsonstore/collections/books';
+const body = document.querySelector('body');
+
+loadBtn.addEventListener('click', loadBooks);
+
+window.onload = (event) => {
+    renderPage([]);
+};
+
+async function loadBooks() {
+    const response = await api.get(URL);
+    const data = Object.values(response);
+    renderPage(data);
+}
+
+function renderPage(data) {
+    render(html`
+        ${createTable(data)}
+        ${createForm()}
+    `, body);
+}
+
+const submitBtn = document.getElementById('submitBtn');
+
+function createTable(data) {
+    return html`
+        <table>
+            <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                ${data.map(item => html`
+                <tr>
+                    <td>${item.title}</td>
+                    <td>${item.author}</td>
+                    <td>
+                        <button>Edit</button>
+                        <button>Delete</button>
+                    </td>
+                </tr>`)}
+            </tbody>
+        </table>
+    `;
+}
+
+function createForm() {
+    return html`
+        <form id="add-form">
+            <h3>Add book</h3>
+            <label>TITLE</label>
+            <input type="text" name="title" placeholder="Title...">
+            <label>AUTHOR</label>
+            <input type="text" name="author" placeholder="Author...">
+            <input id='submitBtn' type="submit" value="Submit">
+        </form>
+    `;
+}
