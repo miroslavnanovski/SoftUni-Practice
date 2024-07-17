2858889d4ec67dda417f0663c73bec410fa28ed9
miroslavnanovski <156704544+miroslavnanovski@users.noreply.github.com>
2024-07-17 18:05:20 +0300

    Update search.js

--- 06.JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
+++ 06.JS-Applications-Client-Side-Rendering-Exercise-Resources/03.Search-in-List/search.js
@@ -4,16 +4,14 @@ import {render,html} from "./node_modules/lit-html/lit-html.js"
 const inputRef = document.getElementById('searchText');
 const townsDiv = document.getElementById('towns');
 const seachBtn = document.querySelector('button');
-const resultDiv = document.getElementById('result')
 
 seachBtn.addEventListener('click',search);
 
-
-let matches = 0;
+let matchCount = 0;
 
 search()
 function search(e) {
-   matches = 0;
+
    let searchText = null;
    if(e){
       searchText = inputRef.value;
@@ -21,18 +19,14 @@ function search(e) {
 
    let temp = towns.map(town => createTemp(town,searchText));
    render(temp,townsDiv)
-   if(matches >= 0){
-      resultDiv.textContent = `${matches} matches found`;
-   }
    
 
 }
 
 function createTemp(town,searchText){
-   let isMatch = town.includes(searchText) && searchText !== '';
-   if(isMatch){
-      matches++;
-   }
+
+   let isMatch = town.includes(searchText);
+   matchCount++;
    return html`
    <ul>
       <li class=${isMatch ? "active" : ""}>${town}</li>
