81be090bce55f6f50316365152c4799f5cc751f9
miroslavnanovski <156704544+miroslavnanovski@users.noreply.github.com>
2024-07-20 14:42:08 +0300

    made a few changes

--- 07.JS-Applications-Routing-Exercise-Resources/01.Furniture/src/app.js
+++ 07.JS-Applications-Routing-Exercise-Resources/01.Furniture/src/app.js
@@ -1,19 +1,20 @@
-import {page} from  "../node_modules/page/page.mjs"
+import page from "../node_modules/page/page.mjs"
+import { showHomeView } from "./views/homeView.js";
 
 
 const root = document.querySelector('.container');
 
 
-    page("/", () => console.log('home'))
-    page( "/catalog",()=> console.log("catalog"))
-    page( "/create",()=> console.log("catalog"))
-    page( "/dashboard",()=> console.log("catalog"))
-    page(  "/details",()=> console.log("catalog"))
-    page(  "/edit",()=> console.log("catalog"))
-    page(  "/login",()=> console.log("catalog"))
-    page(  "/myFurniture",()=> console.log("catalog"))
-    page(   "/register",()=> console.log("catalog"))
-    page(   "/logout",()=> console.log("catalog"))
+//     page("/",showHomeView)
+//     page("/dashboard",showHomeView)
+//     page("/create",()=> console.log("create"))
+//     page("/details",()=> console.log("details"))
+//     page("/edit",()=> console.log("edit"))
+//     page("/login",()=> console.log("login"))
+//     page("/myFurniture",()=> console.log("myFurniture"))
+//     page("/register",()=> console.log("register"))
+//     page("/logout",()=> console.log("logout"))
     
     
 
+// page.start();
