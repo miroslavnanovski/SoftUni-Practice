3fb1127d1c3f80842d931c55bdc2edd3e53a6b17
miroslavnanovski <156704544+miroslavnanovski@users.noreply.github.com>
2024-08-02 02:31:48 +0300

    made home,login and register view

---
+++ JS Applications/Our-Luxury-Cars_Resources/src/views/login.js
@@ -0,0 +1,45 @@
+import { login } from "../data/user.js";
+import { html,render } from "../lib.js";
+import { page } from "../lib.js";
+import { updateNav } from "../util.js";
+import { createSubmitHandler } from "../util.js";
+
+
+const loginTemplate = (onLogin) => html`
+<section id="login">
+<div class="form">
+  <h2>Login</h2>
+  <form @submit =${onLogin} class="login-form">
+    <input type="text" name="email" id="email" placeholder="email" />
+    <input
+      type="password"
+      name="password"
+      id="password"
+      placeholder="password"
+    />
+    <button type="submit">login</button>
+    <p class="message">
+      Not registered? <a href="/register">Create an account</a>
+    </p>
+  </form>
+</div>
+</section>
+`
+
+export function loginView(){
+    render(loginTemplate(createSubmitHandler(onLogin)));
+  
+  }
+  
+  async function onLogin({email, password}){
+  
+    if(!email || !password){
+      return alert('All fields are required!')
+    }
+  
+    await login(email,password);
+  
+    updateNav();
+    page.redirect('/')
+  
+  }
